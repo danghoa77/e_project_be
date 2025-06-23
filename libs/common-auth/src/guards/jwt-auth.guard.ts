ee2996f029ee1c73c68c0b43b64732bab5c6cc1f
Nguyễn Đăng Hòa <danghoabay7@gmail.com>
2025-06-23 11:02:09 +0700

    Update jwt auth guard

--- libs/common-auth/src/guards/jwt-auth.guard.ts
+++ libs/common-auth/src/guards/jwt-auth.guard.ts
@@ -1,26 +1,6 @@
-// user-service/src/auth/guards/jwt-auth.guard.ts
-// Check token valid or not
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+//jwt-auth.guard.ts
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-    handleRequest<TUser = any>(
-        err: any,
-        user: TUser,
-        info: any,
-        _context?: import('@nestjs/common').ExecutionContext,
-        _status?: any
-    ): TUser | never {
-        if (err || !user) {
-            if (info?.name === 'TokenExpiredError') {
-                throw new UnauthorizedException('Token has expired.');
-            }
-            if (info?.name === 'JsonWebTokenError') {
-                throw new UnauthorizedException('Token is not valid.');
-            }
-            throw err || new UnauthorizedException('You need to login to access.');
-        }
-        return user;
-    }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') { }
